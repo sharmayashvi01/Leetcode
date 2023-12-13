a46fad5e77b70d286b199a63ff64ff18c39dfa0b
sharmayashvi01 <134733476+sharmayashvi01@users.noreply.github.com>
2023-12-14 00:43:14 +0530

    Time: 17 ms (54.47%), Space: 13.6 MB (7.11%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,36 +1,70 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int res=0;
+//              APPROACH 1
+//         int res=0;
+//         int m=mat.size();
+//         int n=mat[0].size();
+        
+//         for(int row=0;row<m;row++){
+//             for(int col=0;col<n;col++){
+//                 if(mat[row][col]==0)
+//                     continue;
+                
+//                 bool special=1;
+//                 // check column
+//                 for(int r=0;r<m;r++){
+//                     if(r != row && mat[r][col] == 1){
+//                         special=false;
+//                         break;
+//                     }
+//                 }
+                
+//                 // check row
+//                 for(int c=0;c<n;c++){
+//                     if(c != col && mat[row][c] == 1){
+//                         special=false;
+//                         break;
+//                     }
+//                 }
+                
+//                 if(special)
+//                     res++;
+//             }
+//         }
+//         return res;
+        
+//         ---------------------------------------------------------------------
+//          APPROACH 2
+//         ---------------------------------------------------------------------
+        
         int m=mat.size();
         int n=mat[0].size();
         
+        vector<int> rowCount(m,0);
+        vector<int> colCount(n,0);
+        
         for(int row=0;row<m;row++){
-            for(int col=0;col<n;col++){
-                if(mat[row][col]==0)
-                    continue;
-                
-                bool special=1;
-                // check column
-                for(int r=0;r<m;r++){
-                    if(r != row && mat[r][col] == 1){
-                        special=false;
-                        break;
-                    }
+            for(int col=0;col<n;col++)
+            {
+                if(mat[row][col]==1)
+                {
+                    rowCount[row]++;
+                    colCount[col]++;
+
                 }
-                
-                // check row
-                for(int c=0;c<n;c++){
-                    if(c != col && mat[row][c] == 1){
-                        special=false;
-                        break;
-                    }
-                }
-                
-                if(special)
-                    res++;
             }
         }
+        
+        int res=0;
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(mat[row][col] == 0)
+                    continue;
+                if(rowCount[row] == 1 and colCount[col] == 1)
+                    res++;
+                }
+            }
         return res;
     }
 };
